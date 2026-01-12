b1bf4bb63302222b3c402478ec569cc47496918f
minseok <minseokgyang@gmail.com>
2026-01-12 15:58:27 +0900

    :sparkles: feat(#11) : preference models 작성 및 migrate 생성

---
+++ apps/user/models/preference.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from apps.user.models.user import User
+from apps.game.models.genre import Genre
+
+
+class Preference(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_column="user_id")
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, db_column="genre_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted_at = models.DateTimeField(default=False)
