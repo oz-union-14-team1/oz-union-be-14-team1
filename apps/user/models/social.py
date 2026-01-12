28e6edd4e7c19c2ef7eef9eb7ce5b04d06eebd29
minseok <minseokgyang@gmail.com>
2026-01-12 15:10:09 +0900

    :sparkles: fix(#09) : social모델 1차 피드백 반영

--- apps/user/models/social.py
+++ apps/user/models/social.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-from apps.core.models import TimeStampedModel
 from apps.user.models.user import User
 
 
-class UserSocialAccount(TimeStampedModel):
+class UserSocialAccount():
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     provider = models.CharField(max_length=20)
     provider_user_name = models.CharField(max_length=255)
