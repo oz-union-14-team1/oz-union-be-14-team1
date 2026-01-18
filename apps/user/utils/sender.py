ffb1c23034e647b3e1796271e22ead23710ec33b
minseok <minseokgyang@gmail.com>
2026-01-19 00:32:48 +0900

    :sparkles: feat(#28) : sender 작성

---
+++ apps/user/utils/sender.py
@@ -0,0 +1,17 @@
+import telnyx # type: ignore[attr-defined]
+from django.conf import settings
+
+# Tenlyx에 내 API키로 인증
+telnyx.api_key = settings.TELNYX_API_KEY # type: ignore[attr-defined]
+
+
+class SMSSender:
+    @staticmethod
+    def send_verification_code(phone: str, code: str) -> None:  # SMS 보내는 함수 정의
+        body = f"PlayType 인증 코드: {code}"
+
+        telnyx.Message.create( # type: ignore[attr-defined]
+            from_=settings.TELNYX_FROM_NUMBER,
+            to=phone,
+            text=body,
+        )
