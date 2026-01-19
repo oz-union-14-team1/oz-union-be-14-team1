31089acf51682275b407791da867eb983ed89444
minseok <minseokgyang@gmail.com>
2026-01-19 15:41:06 +0900

    :bug:  fix(#46) : 1차 피드백 반영

--- apps/user/utils/limiter.py
+++ apps/user/utils/limiter.py
@@ -3,9 +3,8 @@ from typing import Optional, Any
 
 from django.core.cache import caches
 
-ONE_MINUTE = 60
-ONE_HOUR = ONE_MINUTE * 60
-ONE_DAY = ONE_HOUR * 24
+from apps.user.exceptions import RateLimitExceeded
+from apps.user.constants import Period
 
 
 class SMSLimiter:
@@ -14,28 +13,40 @@ class SMSLimiter:
         self.namespace = namespace
 
     # Redis key 생성
-    def _key(self, phone: str, period: str) -> str:
+    def _key(self, phone: str, period: Period) -> str:
         phone_normalized = phone.strip()
-        return f"{self.namespace}:{period}:{phone_normalized}"
+        return f"{self.namespace}:{period.value}:{phone_normalized}"
 
     # 이번요청이 허용되는지 확인
-    def can_send(self, phone: str, period: str, limit: int) -> bool:
+    def can_send(self, phone: str, period: Period, limit: int) -> bool:
         key = self._key(phone, period)
         count = self.redis.get(key) or 0
         return int(count) < limit
 
-    # 요청기록 + 카운트 증가 Redis에 키가 없으면 생성 TTL 설정 , Redis에 키가 있으면 incr만 함
-    def record(self, phone: str, period: str, limit: int, ttl: int) -> bool:
-        key = self._key(phone, period)
-        new = self.redis.incr(key)
+    # 요청기록 + 제한 확인 기록 후에 카운트증가 ( Redis에 키가 없으면 생성 TTL 설정 , Redis에 키가 있으면 incr만 함)
+    def record(self, phone: str, period: Period, limit: int, ttl: int) -> bool:
+        try:
+            key = self._key(phone, period)
+            new = self.redis.incr(key)
 
-        if new == 1:
-            self.redis.expire(key, ttl)
+            if new == 1:  # 처음 생성
+                self.redis.expire(key, ttl)
 
-        return new <= limit
+            if new > limit:  # 제한 초과 시
+                retry_after = self.get_remaining(phone, period)
+                raise RateLimitExceeded(
+                    message=f"SMS 발송 한도를 초과했습니다. {retry_after}초 후에 다시 시도해주세요.",
+                    retry_after=retry_after,
+                )
+            return True
+
+        except RateLimitExceeded:
+            raise
+        except Exception as e:
+            raise Exception(f"SMS 발송 기록 중 오류 발생: {str(e)}")
 
     # 남은 TTL 조회 (초 단위로 조회)
-    def get_remaining(self, phone: str, period: str) -> Optional[int]:
+    def get_remaining(self, phone: str, period: Period) -> Optional[int]:
         key = self._key(phone, period)
         ttl_func = getattr(self.redis, "ttl", None)
         if ttl_func is None:
