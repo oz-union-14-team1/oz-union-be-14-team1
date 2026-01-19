dcda769a093b2ddf4067b3c81cbb29cf457265e2
minseok <minseokgyang@gmail.com>
2026-01-19 14:09:41 +0900

    :sparkles: feat(#46) : User model 에서 validator 분리 작업

---
+++ apps/user/validators/__init__.py
@@ -0,0 +1,11 @@
+from .validator import (
+    validate_email_format,
+    validate_phone_format,
+    validate_nickname_format,
+)
+
+__all__ = [
+    "validate_email_format",
+    "validate_phone_format",
+    "validate_nickname_format",
+]
