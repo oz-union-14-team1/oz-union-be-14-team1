143d746319222bc3e5fecacdc33c6e73526e603e
kihoon <nike6736@naver.com>
2026-01-26 15:18:14 +0900

    💡 chore(#82): black 진행

--- apps/user/services/preference_list_service.py
+++ apps/user/services/preference_list_service.py
@@ -5,4 +5,4 @@ from apps.user.models.preference import Preference
 
 def get_user_preferences(user: User):
     # N+1 문제 방지를 위해 select_related 사용
-    return Preference.objects.filter(user=user).select_related("genre") # type: ignore
+    return Preference.objects.filter(user=user).select_related("genre")  # type: ignore
