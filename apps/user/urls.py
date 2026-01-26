ce288251387d3ac5def1fe10f0e508fe9f715f29
minseok <minseokgyang@gmail.com>
2026-01-26 17:37:05 +0900

    :sparkles: feat(#81)/ 내정보 urls 작성
    # Conflicts:
    #	apps/user/urls.py

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from apps.user.views.LoginView import LoginView
 from apps.user.views.preference.preference_api import PreferenceAPIView
 from apps.user.views.registerview import RegisterView
+from apps.user.views.profileview import MeView
 
 app_name = "user"
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("signup/", RegisterView.as_view(), name="signup"),
+    path("me/", MeView.as_view(), name="profile"),
     path("preference/", PreferenceAPIView.as_view(), name="preference_create"),
 ]
