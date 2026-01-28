443ebddf4a0993b46cc71dfedad77ae5c5427028
sowon-bak <cruzean479@gmail.com>
2026-01-28 17:02:20 +0900

    :sparkles: feat(#96):위시리스트 작성 중

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from apps.game.views.game_views import GameListView, GameDetailView
-from apps.game.views.wishlist_views import WishlistView, WishlistDetailView
-
+from apps.game.views.wishlist_views import WishlistView, WishlistDestroyView
 
 urlpatterns = [
     path('', GameListView.as_view(), name='game-list'),
     path('<int:pk>/', GameDetailView.as_view(), name='game-detail'),
     path('wishlist/', WishlistView.as_view(), name='wishlist'),
-    path('wishlist/<int:pk>/', WishlistDetailView.as_view(), name='wishlist-detail'),
+    path('wishlist/<int:pk>/', WishlistDestroyView.as_view(), name='wishlist-detail'),
 
 ]
