7f9e40bbb4cc2ecca589affeca98e85700407db3
sowon-bak <cruzean479@gmail.com>
2026-01-28 13:39:10 +0900

    :sparkles: feat(#96):충돌 해결 및 intro 번역기 추가

---
+++ apps/game/serializers/wishlist_serializer.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from apps.game.models.wishlist import Wishlist
+
+
+class WishlistCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Wishlist
+        fields = ["game"]
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        game = validated_data["game"]
+
+        # 이미 위시리스트에 있는지 확인
+        wishlist, created = Wishlist.objects.get_or_create(user=user, game=game)
+
+        return wishlist
+
+
+class WishlistSerializer(serializers.ModelSerializer):
+    """위시리스트 조회용 Serializer"""
+
+    game_name = serializers.CharField(source="game.name", read_only=True)
+    game_image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Wishlist
+        fields = ["id", "game", "game_name", "game_image", "created_at"]
+
+    def get_game_image(self, obj):
+        first_image = obj.game.game_images.first()
+        if first_image:
+            return first_image.img_url
+        return None
