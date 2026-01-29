9991e3457ee923bdae2a824e809102d1ba21fda6
sowon-bak <cruzean479@gmail.com>
2026-01-29 10:50:20 +0900

    :sparkles: feat(#96):태그 및 장르 한글 스키마 추가

--- apps/game/models/tag.py
+++ apps/game/models/tag.py
@@ -4,6 +4,7 @@ from django.db import models
 class Tag(models.Model):
     tag = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, unique=True)
+    tag_ko = models.CharField(max_length=255, unique=True)
 
     class Meta:
         db_table = "tag"
