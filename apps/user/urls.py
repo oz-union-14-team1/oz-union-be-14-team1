0b022cc5827a799feffef1f273007d21f221a0bc
minseok <minseokgyang@gmail.com>
2026-01-29 17:52:37 +0900

    :bug: fix(#92)/ urls.py에 / 제거

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -11,18 +11,18 @@ from apps.user.views.account_recovery import (
 app_name = "user"
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
-    path("signup/", RegisterView.as_view(), name="signup"),
-    path("me/", MeView.as_view(), name="profile"),
-    path("me/delete/", WithdrawView.as_view(), name="profile_delete"),
-    path("find-account/", FindAccountView.as_view(), name="find_account"),
+    path("login", LoginView.as_view(), name="login"),
+    path("signup", RegisterView.as_view(), name="signup"),
+    path("me", MeView.as_view(), name="profile"),
+    path("me/delete", WithdrawView.as_view(), name="profile_delete"),
+    path("find-account", FindAccountView.as_view(), name="find_account"),
     path(
-        "password/reset/request/",
+        "password/reset/request",
         PasswordResetRequestView.as_view(),
         name="password_reset_request",
     ),
     path(
-        "password/reset/confirm/",
+        "password/reset/confirm",
         PasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
