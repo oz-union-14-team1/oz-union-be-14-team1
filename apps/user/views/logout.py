c12b20f33bdbc8eef961ba12f8d6e5c768249466
minseok <minseokgyang@gmail.com>
2026-02-02 13:40:12 +0900

    :sparkles: feat(#104)/ logout views 작성

---
+++ apps/user/views/logout.py
@@ -0,0 +1,30 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import extend_schema,OpenApiResponse
+
+from apps.user.utils.tokens import TokenService
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        summary="로그아웃",
+        description="Refresh Token , Access Token 을 블랙리스트 처리하고 쿠키를 삭제합니다.",
+        responses={
+            200: OpenApiResponse(
+                response={"type": "object", "properties": {"detail": {"type": "string"}}},
+                description="로그아웃 성공",
+            )
+        },
+    )
+    def post(self, request):
+        refresh_token = request.COOKIES.get("refresh_token")
+
+        if refresh_token:
+            TokenService().black_list(refresh_token)
+
+        response = Response({"detail": "로그아웃 되었습니다."}, status=status.HTTP_200_OK)
+        response.delete_cookie("refresh_token")
+        return response
