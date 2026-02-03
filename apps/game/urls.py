2f95956da65a37d0e20010c6d6446f60e43f5055
sowon-bak <cruzean479@gmail.com>
2026-02-03 17:09:26 +0900

    :sparkles: feat(#128):위시리스트 기반 게임 추천 api 작성

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from apps.game.views.game_views import GameListView, GameDetailView, GameSearchView
 from apps.game.views.wishlist_views import WishlistView, WishlistDestroyView
 from apps.game.views.import_views import GameImportView
-from apps.game.views.recommend_views import GamePreferenceGameRecommendView
+from apps.game.views.recommend_views import (
+    GamePreferenceGameRecommendView,
+    GamePreferenceTagRecommendView,
+)
 
 
 urlpatterns = [
@@ -16,5 +19,10 @@ urlpatterns = [
         "recommend/preference",
         GamePreferenceGameRecommendView.as_view(),
         name="game-recommend-preference",
+    ),
+    path(
+        "api/v1/game/recommend/wishlist",
+        GamePreferenceTagRecommendView.as_view(),
+        name="game-recommend-wishlist",
     ),
 ]
